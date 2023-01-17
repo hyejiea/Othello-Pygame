2d82787f5910c5c7a56f10ade7431871752aa973
hyejiea <aj.chii@yahoo.co.uk>
2023-01-17 17:22:04 +0800

    final output

---
+++ othello/othelloGUI.py
@@ -0,0 +1,82 @@
+import pygame
+
+from othelloAI import *
+from Othello import*
+
+def main():
+
+    # set parameters
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 680
+    player_color = 2  # black
+
+    # init
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption('Player VS AI')
+
+    # load images
+    images = Images()
+
+    # init chessboard
+    chessboard = Chessboard()
+
+    # init tree
+    node = ChessboardTreeNode(chessboard)
+    chessboardTree = ChessboardTree(node)
+    chessboardTree.expandTree()
+
+    draw(screen, images, chessboard)
+    pygame.display.update()
+
+    # main loop
+    while True:
+
+        # catch events
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                set_i = set_j = -1
+                if chessboard.offense == player_color:
+                    px, py = pygame.mouse.get_pos()
+                    set_i = (py - chessboard.margin) // chessboard.width
+                    set_j = (px - chessboard.margin) // chessboard.width
+                # else:
+                #     set_i, set_j = chessboardTree.findBestChess(player_color)
+                if (set_i, set_j) in chessboard.available:
+                    chessboardTree.root = chessboardTree.root.kids[(
+                        set_i, set_j)]
+                    chessboard = chessboardTree.root.chessboard
+                    # update screen
+                    draw(screen, images, chessboard)
+                    pygame.display.update()
+                    # expand only 1 layer
+                    chessboardTree.expandTree()
+
+                    # ai to move
+                    set_i, set_j = chessboardTree.findBestChess(player_color)
+                    chessboardTree.root = chessboardTree.root.kids[(
+                        set_i, set_j)]
+                    chessboard = chessboardTree.root.chessboard
+                    # update screen
+                    draw(screen, images, chessboard)
+                    pygame.display.update()
+                    # expand only 1 layer
+                    chessboardTree.expandTree()
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_b:
+                    if chessboardTree.root.parent:
+                        chessboardTree.root = chessboardTree.root.parent
+                        chessboard = chessboardTree.root.chessboard
+                # update screen
+                draw(screen, images, chessboard)
+                pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
